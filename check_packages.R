9d95c1fd46f0718d55edb8227f2a4cef9fcab7f4
fzou <fuyu_zou@brown.edu>
2017-12-13 11:45:25 -0500

    added check_packages.R

--- check_packages.R
+++ check_packages.R
@@ -1,11 +1,9 @@
 pkgs <- c("shiny","tidyverse","racecar", "shinythemes")
 
-pkgTest <- function(pkgs)
-{ for (i in pkgs){
-  if (!require(i,character.only = TRUE))
-  {
-    install.packages(x,dep=TRUE)
-    if(!require(i,character.only = TRUE)) stop("Package not found")
+pkgTest <- function(pkgs){ 
+  for (i in pkgs){
+    if (!require(i,character.only = TRUE)){
+      install.packages(i,dep=TRUE)
     }
   }
 }
